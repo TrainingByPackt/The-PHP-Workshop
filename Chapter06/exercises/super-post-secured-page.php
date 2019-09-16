b42aad465990b7cb851b3080f58747c3202b13ca
Rutuja Yerunkar <rutujay@packt.com>
2019-09-16 12:05:16 +0530

    Updated

--- Chapter06/exercises/super-post-secured-page.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-echo sprintf('<p>Welcome, %s!</p>', $_SESSION['username']);
-echo sprintf('<p>Session data: </p>
-<pre>%s</pre>', var_export($_SESSION, true));
-echo '<p><a href="super-post-secured.php?do=logout">Logout</a></p>';
